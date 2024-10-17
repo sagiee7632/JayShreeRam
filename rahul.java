2388fab9dffa34a8a9c6ab46d7170b2918276e49
dac <dac@students.acts.in>
2024-10-17 12:08:07 +0530

    First Commit

---
+++ rahul.java
@@ -0,0 +1,17 @@
+public class rahul{
+public static void main(String [] args){
+
+Integer a = 10;
+Integer b= 15;
+Integer c= 5;
+
+if(a> b && a>c){
+System.out.println(" a is maximum" + a);
+}else if(b > a && b >c){
+System.out.println(" b is maximum" + b);
+}else{
+System.out.println(" c is maximum" + c);
+}
+
+}
+}
